b3f86d6584dfc37ec990688845114886f1487792
Ferdynand Naczynski <nipsufn+gh@pm.me>
2023-07-03 19:44:56 +0200

    revert last

--- connection/connection.go
+++ connection/connection.go
@@ -13,7 +13,6 @@ import (
 )
 
 const Port = "38899"
-var TimeoutSeconds = 30
 
 func SendUdpMessage(host string, message *models.RequestPayload) (*models.ResponsePayload, error) {
 	var (
@@ -33,7 +32,7 @@ func SendUdpMessage(host string, message *models.RequestPayload) (*models.Respon
 		log.Errorf(`Unable to dial up to udp: %s`, err)
 		return nil, err
 	}
-	if err = conn.SetReadDeadline(time.Now().Add(TimeoutSeconds * time.Second)); err != nil {
+	if err = conn.SetReadDeadline(time.Now().Add(30 * time.Second)); err != nil {
 		log.Errorf(`Unable to set UDP timeout: %s`, err)
 		return nil, err
 	}
