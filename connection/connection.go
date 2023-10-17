6cf00e0e1c9988459013c1c25c705e4ac7884ee7
nipsufn <nipsufn+gh@pm.me>
2023-10-17 18:44:28 +0200

    chore: heck logic

--- connection/connection.go
+++ connection/connection.go
@@ -33,7 +33,7 @@ func SendUdpMessage(host string, message *models.RequestPayload) (*models.Respon
 		log.Errorf(`Unable to dial up to udp: %s`, err)
 		return nil, err
 	}
-	if err = conn.SetReadDeadline(time.Now().Add(TimeoutMs * time.Millisecond)); err != nil {
+	if err = conn.SetReadDeadline(time.Now().Add(time.Duration(TimeoutMs) * time.Millisecond)); err != nil {
 		log.Errorf(`Unable to set UDP timeout: %s`, err)
 		return nil, err
 	}
